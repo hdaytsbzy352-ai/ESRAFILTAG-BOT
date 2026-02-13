f3acffc4baf7d8d3e339d24600a56729ab0e6487
hdaytsbzy352-ai <hdaytsbzy352@gmail.com>
2026-02-14 02:33:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,5 +57,8 @@ https://rubika.ir/pov_YaDeGaR/BEJJBHJFAJIGIFEF
 
     return "ok"
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
